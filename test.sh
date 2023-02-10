8c51721f3138b2b535e1ce47473385bb10f9a8d5
akinya1974 <akinya@inbox.ru>
2023-02-10 18:06:07 +0300

    Add Changes test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
 # Всем привет))) моя музыка тут:  https://www.beatport.com/artist/sergiy-akinshin/722907
+
+# Моя группа в VK ДОБАВЛЯЙТЕСЬ)))  https://vk.com/sergiyakinshin
+
+#  https://vk.com/sergiy.akinshin
